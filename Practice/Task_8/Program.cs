0ee1cb787966365f26e6977e21b7fb37fbfcd605
Vitaliy Khlebalov <63170601+vokhlebalov@users.noreply.github.com>
2020-06-15 01:16:58 +0500

    Add files via upload

---
+++ Practice/Task_8/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Task_8
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Graph
+    {
+        public string Name { get; set; }
+        public List<Node> Nodes { get; set; } = new List<Node>();
+
+        public Graph(string name, List<Node> nodes)
+        {
+            Name = name;
+            Nodes = nodes;
+        }
+
+        public Graph(string name)
+        {
+            Name = name;
+        }
+        
+        public override string ToString()
+        {
+            string nodes = Nodes.Aggregate("", (current, node) => current + (node + "\n"));
+            return Name + "\n" + nodes;
+        }
+    }
+    
+    public class Node
+    {
+        public List<Node> Ribs { get; set; } = new List<Node>();
+        public string Name { get; set; }
+
+        public Node(string name, List<Node> ribs)
+        {
+            Name = name;
+            Ribs = ribs;
+        }
+
+        public Node(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            string ribs = Ribs.Aggregate("", (current, node) => current + (node.Name + " "));
+            return $"{Name}: {ribs}";
+        }
+    }
+}
---
+++ Practice/Task_9/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace Task_9
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Введите натуральное число N, равное количеству элементов в списке: ");
+            int n = int.Parse(Console.ReadLine());
+            List list = new List(n);
+            Console.Write("\nЭлементы списка: ");
+            foreach (int element in list)
+            {
+                Console.Write($"{element} ");
+            }
+            
+            Console.Write("\nВведите элемент, который нужно удалить: ");
+            int delete = int.Parse(Console.ReadLine());
+            list.Remove(delete);
+            
+            Console.Write("Элементы списка после удаления: ");
+            foreach (int element in list)
+            {
+                Console.Write($"{element} ");
+            }
+            
+            Console.Write("\nВведите элемент, который нужно найти: ");
+            int find = int.Parse(Console.ReadLine());
+            if (list.Contains(find))
+            {
+                Console.WriteLine("\nСписок содержит данный элемент");
+            }
+            else
+            {
+                Console.WriteLine("\nСписок не содержит данный элемент");
+            }
+        }
+    }
+
+    class Node
+    {
+        public Node Next { get; set; }
+        public int Data { get; set; }
+
+        public Node(int data)
+        {
+            Data = data;
+        }
+    }
+
+    class List : IEnumerable<int>
+    {
+        public Node Head { get; set; }
+
+        public int Count
+        {
+            get
+            {
+                int counter = 0;
+                foreach (int element in this)
+                {
+                    counter++;
+                }
+
+                return counter;
+            }
+        }
+
+        public List(int size)
+        {
+            Head = new Node(1);
+            Head.Next = Head;
+            Node current = Head;
+            for (int i = 2; i <= size; i++)
+            {
+                current.Next = new Node(i);
+                current = current.Next;
+                current.Next = Head;
+            }
+        }
+
+        public void Remove(int data)
+        {
+            if (Count == 0)
+            {
+                return;
+            }
+
+            if (Count == 1 && Head.Data == data)
+            {
+                Head = null;
+                return;
+            }
+
+            if (Count == 1 && Head.Data != data)
+            {
+                Console.WriteLine("Элемента нет в списке");
+            }
+
+            if (Head.Data == data)
+            {
+                Head = Head.Next;
+            }
+
+            Node node = Head;
+            while (node.Next.Next != null && node.Next.Data != data)
+                node = node.Next;
+
+            if (node.Next.Data != data)
+            {
+                Console.WriteLine("Элемента нет в списке");
+                return;
+            }
+
+            node.Next = node.Next.Next;
+            Console.WriteLine("Элемент успешно удален!");
+        }
+
+        public bool Contains(int data)
+        {
+            foreach (int element in this)
+            {
+                if (element == data)
+                    return true;
+            }
+
+            return false;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            Node current = Head;
+            do
+            {
+                yield return current.Data;
+                current = current.Next;
+            } while (current != Head);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
